35d729ce816bb15634e197c6dc3ed3989fe00b3a
neto test <kharicheva33@mail.ru>
2023-03-05 23:02:49 +0300

    task3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,17 +3,17 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    product = {id: 1, title: 'Пицца', count: 5, price: '500.00'};
+    
     
 
-    let sum = product.count * product.price;
-    let itemCountText=" ";
+    
+    let itemCountText = ``;
     itemCountText = itemCountText + product.count; 
-    itemCountText =  ` × `; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+    itemCountText +=  ` × `; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     itemCountText += product.price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText = itemCountText + ` ₽ = `; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    itemCountText = itemCountText + sum;// 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+    itemCountText += sum;// 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = itemCountText + ` ₽`;// 8 . Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
     // Конец решения задания №3.1.
