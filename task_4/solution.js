3020f0aa229c76afb55473a4b96af0ee3b654f8d
neto test <kharicheva33@mail.ru>
2023-03-10 11:00:24 +0300

    vvv

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,15 +1,11 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-    
     let data = JSON.parse(json);
-    let products = data.products;
-    return products;
-    
+    return data.products;
 }
 
 function renderProductsCards(json) {
-
 clearProducts();
 let products = parseProducts(json);
 products = parseProducts(json);
