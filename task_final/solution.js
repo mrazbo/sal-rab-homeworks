9b4f8a509b7c569bca1e184885b3b022059e7c67
neto test <kharicheva33@mail.ru>
2023-03-14 08:51:26 +0300

    hghg22

--- task_final/solution.js
+++ task_final/solution.js
@@ -8,6 +8,6 @@ function sendRequest(name, phone, address, goods, sum) {
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
     data.client = 'Иван';
-    let jsonData = JSON.stringify({data: data});
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
