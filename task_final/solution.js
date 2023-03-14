a52b682c6e9f19c7610ea92c4b83b3fa63770b4e
neto test <kharicheva33@mail.ru>
2023-03-14 09:06:52 +0300

    hghg3

--- task_final/solution.js
+++ task_final/solution.js
@@ -3,7 +3,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = { client: name + " " + phone, order: { address, sum }, goods: [] };
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i += 1) {
-    data.goods.push(goods[i].title);
+    data.goods.push({title:goods[i].title, count: goods[i].count});
     }
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
